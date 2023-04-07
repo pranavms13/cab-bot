883a823f955d005181ba34eafbbac6661cb5e4c1
Sandeep Kumar <dower.loess-0l@icloud.com>
2023-04-07 15:24:59 +0530

    Test API

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-import Express from "express";
+import Express, { NextFunction, Request, Response } from "express";
 import DB from "./utils/db";
 import logger from "./utils/logger";
 import Cache from "./utils/cache";
@@ -11,6 +11,17 @@ async function main() {
     await new Cache().initialize();
 
     const app = Express();
+
+    app.use(Express.json());
+
+    app.get("/api/whatsapp/v1/webhook", async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        console.log(req.params);
+        res.status(200).send(req.params.hub_challenge);
+      } catch (error) {
+        next(error);
+      }
+    })
 
     app.listen(process.env.PORT || 3000, () => {
       logger.info(`Server started on port ${process.env.PORT || 3000}`);
