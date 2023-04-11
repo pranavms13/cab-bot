1f0b0a769889070b7d7402c5e0b8214a9f1e3d94
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-04-11 12:05:06 +0530

    Respond only to messages

--- src/handlers/whatsapp.ts
+++ src/handlers/whatsapp.ts
@@ -16,18 +16,20 @@ export async function onMessageReceivedHandler(req: Request, res: Response, next
       ...req.body,
       timestamp: new Date(),
     });
-    const response = await whatsappAxiosInstance.post("/messages", {
-      messaging_product: "whatsapp",
-      recipient_type: "individual",
-      to: req.body.entry[0].changes[0].value.contacts[0].wa_id,
-      type: "template",
-      template: {
-        name: "welcome_message",
-        language: {
-          code: "en",
+    if(req.body.entry[0].changes[0].value.messages) {
+      const response = await whatsappAxiosInstance.post("/messages", {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: req.body.entry[0].changes[0].value.contacts[0].wa_id,
+        type: "template",
+        template: {
+          name: "welcome_message",
+          language: {
+            code: "en",
+          },
         },
-      },
-    });
+      });
+    }
     res.status(200).json({
       status: "ok",
     });
