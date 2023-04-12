7fba872e17687f994667d60fcb64e3b6e26855d1
Sandeep Kumar <sandeepmahendran@gmail.com>
2023-04-12 16:00:58 +0530

    Find Nearby Autos flow added

--- src/handlers/whatsapp.ts
+++ src/handlers/whatsapp.ts
@@ -4,6 +4,7 @@ import { whatsappAxiosInstance } from "../utils/whatsapp";
 import State, { Platform } from "../models/state";
 import { AxiosError } from "axios";
 import logger from "../utils/logger";
+import { faker } from "@faker-js/faker"
 
 export async function verifyWebhook(req: Request, res: Response, next: NextFunction) {
   try {
@@ -27,6 +28,7 @@ export async function onMessageReceivedHandler(req: Request, res: Response, next
       // Constants from Whatsapp Update
       const waId = update.entry[0].changes[0].value.contacts[0].wa_id;
       const messageType = update.entry[0].changes[0].value.messages[0].type;
+      const selectedButtonMessageContent = update.entry[0].changes[0].value.messages[0].button?.text;
       
       // Metadata for further processing
       let templateId : string | null = null;
@@ -41,7 +43,8 @@ export async function onMessageReceivedHandler(req: Request, res: Response, next
         currentState = new State(Platform.Whatsapp, waId, 0, 1, {
           pickupLocation: null,
           dropLocation: null,
-          pickupDate: null,
+          contactNumber: null,
+          isOnlyList: false,
         });
         templateId = "welcome_message";
         rawMessage = null;
@@ -52,9 +55,12 @@ export async function onMessageReceivedHandler(req: Request, res: Response, next
               rawMessage = "Oh no! That's a wrong response. Please try again!";
             } else {
               templateId = "request_pickup_location";
-              currentState.nextStep = 2;
+              currentState.nextStep = 2 ;
               currentState.previousStep = 1;
               rawMessage = null;
+              if(selectedButtonMessageContent.split(".")[0] == "2") {
+                currentState.metaData.isOnlyList = true;
+              }
             }
             break;
 
@@ -64,10 +70,28 @@ export async function onMessageReceivedHandler(req: Request, res: Response, next
             }
             else {
               currentState.metaData.pickupLocation = update.entry[0].changes[0].value.messages[0].location;
-              templateId = "request_drop_location";
-              currentState.nextStep = 3;
-              currentState.previousStep = 2;
-              rawMessage = null;
+              if(currentState.metaData.isOnlyList) {
+                templateId = null;
+                rawMessage = "List of Autos nearby:\n"
+                for(let i = 0; i < 5; i++) {
+                  const driverName = faker.name.fullName()
+                  const distance = `${faker.random.numeric(1, {
+                    allowLeadingZeros: false,
+                    bannedDigits: ["2", "3", "4", "5", "6", "7", "8", "9"],
+                  })}.${faker.random.numeric(2, {
+                    allowLeadingZeros: true,
+                  })}`;
+                  const phone = faker.phone.number("+91 ##########")
+                  rawMessage += `${i+1}. ${driverName} - ${distance} km away - ${phone}\n`
+                }
+                currentState.nextStep = 0;
+                currentState.previousStep = 0;
+              } else {
+                templateId = "request_drop_location";
+                rawMessage = null;
+                currentState.nextStep = 3;
+                currentState.previousStep = 2;
+              }
             }
             break;
 
@@ -140,7 +164,11 @@ export async function onMessageReceivedHandler(req: Request, res: Response, next
         }
       }
 
-      currentState.saveState();
+      if(currentState.nextStep === 0 && currentState.previousStep === 0) {
+        currentState.clearCache()
+      } else {
+        currentState.saveState();
+      }
       await sendWhatsappMessage(waId, templateId, rawMessage, components);
     }
 
--- src/models/state.ts
+++ src/models/state.ts
@@ -20,9 +20,16 @@ export default class State {
       longitude: number;
     } | null;
     contactNumber: string | null;
+    isOnlyList: boolean;
   };
 
-  constructor(platform: Platform, userId: string, previousStep: number, nextStep: number, metaData: any) {
+  constructor(
+    platform: Platform,
+    userId: string,
+    previousStep: number,
+    nextStep: number,
+    metaData: any
+  ) {
     this.platform = platform;
     this.userId = userId;
     this.previousStep = previousStep;
@@ -30,18 +37,15 @@ export default class State {
     this.metaData = metaData;
   }
 
-  async saveState() : Promise<void> {
-    await Cache.getCache().hset(
-      this.userId,
-      {
-        ...this,
-        metaData: JSON.stringify(this.metaData),
-      }
-    );
+  async saveState(): Promise<void> {
+    await Cache.getCache().hset(this.userId, {
+      ...this,
+      metaData: JSON.stringify(this.metaData),
+    });
     await Cache.getCache().expire(this.userId, parseInt(process.env.TTL!) * 60);
   }
 
-  static async fetchFromCache(key: string) : Promise<State | null> {
+  static async fetchFromCache(key: string): Promise<State | null> {
     let data = await Cache.getCache().hgetall(key);
     if (Object.keys(data).length > 0) {
       return new State(
@@ -49,13 +53,17 @@ export default class State {
         data.userId,
         parseInt(data.previousStep),
         parseInt(data.nextStep),
-        data.metaData? JSON.parse(data.metaData) : {}
+        data.metaData ? JSON.parse(data.metaData) : {}
       );
     }
-    return null
+    return null;
   }
 
-  static async clearAllCache() : Promise<void> {
+  static async clearAllCache(): Promise<void> {
     await Cache.getCache().flushall();
+  }
+
+  async clearCache(): Promise<void> {
+    await Cache.getCache().del(this.userId);
   }
 }
